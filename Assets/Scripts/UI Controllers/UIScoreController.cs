b61399847d55d42f9512aa526d4c62bd286fdb51
Yohans Nascimento <yohans.dev@gmail.com>
2023-09-28 14:49:38 -0300

    feat: added XP system + XP UI Bar

--- Assets/Scripts/UI Controllers/UIScoreController.cs
+++ Assets/Scripts/UI Controllers/UIScoreController.cs
@@ -8,20 +8,49 @@ using UnityEngine.SocialPlatforms.Impl;
 public class UIScoreController : MonoBehaviour
 {
     public TMP_Text scoreText;
-    private int totalScore = 0;
+    private int currentScore = 0;
+    private int currentXp = 0;
+    private int maxXp = 10000;
+    public Slider xpSlider;
 
     private void Start()
     {
+        xpSlider.maxValue = maxXp;
+        xpSlider.value = currentXp;
         GameEvents.current.onPlayerScoredTrigger += OnPlayerScored;
     }
 
     private void OnPlayerScored(int score) //Logica do evento
     {
-        totalScore += score;
+        currentScore += score;
+        currentXp += score;
+
+        if(currentXp > maxXp)
+        {
+            Debug.Log("LEVEL UP!");
+            ResetMaxXp(maxXp + maxXp / 2); //Trocar essa formula par auma mais balanceada.
+        }
+        else
+        {
+            SetXp(currentXp);
+        }
+    }
+
+    private void ResetMaxXp(int maxXp) //O maximo da barra de xp reseta e aumenta assim que o player chegar no seu limite maximo.
+    {
+        xpSlider.maxValue = maxXp;
+        currentXp = 0;
+        xpSlider.value = currentXp;
+        this.maxXp = maxXp;
+    }
+
+    private void SetXp(int xp)
+    {
+        xpSlider.value = xp;
     }
 
     private void Update()
     {
-        scoreText.text = "SCORE: " + totalScore.ToString();
+        scoreText.text = "SCORE: " + currentScore.ToString();
     }
 }
